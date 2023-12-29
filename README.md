e5365191b0c4d5d0ef8f57fc74c43bd9156c0d4a
ILYECHAkR <i.bouchaker@univ-boumerdes.dz>
2023-12-29 23:51:39 +0100

    prog1 version 0.2 - add sorting option

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Python_codes
 My Python Code Repository
+
+This repository contains a collection of Python scripts and projects created by me. The code covers a variety of topics, including basics in data analysis, machine learning, web development.
--- prog1.py
+++ prog1.py
@@ -1,5 +1,15 @@
 #sort a list 
 
+def func1 (l):
+    print("func1 ")
+    l.sort()
+    return l
+
+def func2 (l) :
+    print("func2 ")
+    l.sort(reverse = True)
+    return l
+
 print("This program is for sort a list")
 nb = int(input("what is the number of Items : "))
 
@@ -10,11 +20,23 @@ list = []
 
 print("Enter the list items (separated by spaces):")
 items_str = input()
-items = items_str.split()
+items = items_str.split(" ")
 
 for i in range(nb):
     item = items[i]  # Access items from the input list
-    print("Item", i + 1, ":", item)  # Print item with index
+    #print("Item", i + 1, ":", item)  # Print item with index
     list.append(item)
 
-print("Final list:", list)
+print("The list :", list)
+
+print("Choose one option below :")
+print("1 - Ascending sort")
+print("2 - Descending sort")
+choice  = input()
+
+if choice == "1":
+   list_ADsorted = func1(list)
+elif choice == "2":
+   list_ADsorted = func2(list)
+
+print("List sorted : " , list_ADsorted)
