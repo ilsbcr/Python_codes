ff38304417c88e614b79270e875c46f47408de6d
ILYECHAkR <i.bouchaker@univ-boumerdes.dz>
2024-01-19 14:18:28 +0100

    prog3_v0.2

--- prog3.py
+++ prog3.py
@@ -1,13 +1,18 @@
 import random
 
+range_Top = int(input("Enter the top value of the range : "))
+range_Bottom = int(input("Enter the bottom value of the range : "))
 number = int(input("Enter a number :"))
-random_number = random.randint(1,10)
 
-if(random_number == number) :
+
+random_Number = random.randint(range_Bottom,range_Top)
+
+if(random_Number == number) :
     print("Correct !")
 
 else :
     print("NOO")
 
-    print("Given number : ", number)
-    print("Guessed number :",random_number)
+
+print("Given number : ", number)
+print("Guessed number :",random_Number)
