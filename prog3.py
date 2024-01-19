0270c297e9865023da620b85adf1efb8f00e6757
ILYECHAkR <i.bouchaker@univ-boumerdes.dz>
2024-01-19 14:29:52 +0100

    prog3_v0.3

--- prog3.py
+++ prog3.py
@@ -1,18 +1,28 @@
 import random
 
-range_Top = int(input("Enter the top value of the range : "))
-range_Bottom = int(input("Enter the bottom value of the range : "))
-number = int(input("Enter a number :"))
+choice = "y"
+
+while choice.lower() == "y" :
+
+    range_Top = int(input("Enter the top value of the range : "))
+    range_Bottom = int(input("Enter the bottom value of the range : "))
+    number = int(input("Enter a number :"))
 
 
-random_Number = random.randint(range_Bottom,range_Top)
+    random_Number = random.randint(range_Bottom,range_Top)
 
-if(random_Number == number) :
-    print("Correct !")
+    if(random_Number == number) :
+        print("Correct ! , you are lucky , probalility of the correct number :" , 1 / ((range_Bottom +  range_Top )-1) )
 
-else :
-    print("NOO")
+    else :
+        print("NO !")
 
 
-print("Given number : ", number)
-print("Guessed number :",random_Number)
+    print("Given number : ", number)
+    print("Guessed number :",random_Number)
+
+    choice = input("Do you want to continue y/n : ")
+    if (choice.lower == "n") : 
+        break
+
+print("Game ended")
