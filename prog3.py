ee98e37591f708325ac0b62f7e26bd2f3e0e74f8
ILYECHAkR <i.bouchaker@univ-boumerdes.dz>
2024-01-19 14:43:24 +0100

    prog3_v1

--- prog3.py
+++ prog3.py
@@ -6,16 +6,25 @@ while choice.lower() == "y" :
 
     range_Top = int(input("Enter the top value of the range : "))
     range_Bottom = int(input("Enter the bottom value of the range : "))
+    while True :
+        if  range_Bottom > range_Top : 
+            print("range_Bottom > range_Top ! Reenter ranges ")
+            range_Top = int(input("Enter the top value of the range : "))
+            range_Bottom = int(input("Enter the bottom value of the range : "))
+
+        else : 
+            break
     number = int(input("Enter a number :"))
 
 
     random_Number = random.randint(range_Bottom,range_Top)
+    Propability = 1 / ((range_Top - range_Bottom )+1) 
 
     if(random_Number == number) :
-        print("Correct ! , you are lucky , probalility of the correct number :" , 1 / ((range_Bottom +  range_Top )-1) )
+        print("Correct ! , you are lucky , probalility of the correct number :" , Propability )
 
     else :
-        print("NO !")
+        print("NO ! probalility of the wrong number :" ,1 - Propability)
 
 
     print("Given number : ", number)
