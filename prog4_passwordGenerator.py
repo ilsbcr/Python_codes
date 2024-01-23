0081bac74eb15112ce0140b81cf993dbb7e0388f
ILYECHAkR <i.bouchaker@univ-boumerdes.dz>
2024-01-23 19:30:37 +0100

    prog4_passwordGeneratorV1

--- prog4_passwordGenerator.py
+++ prog4_passwordGenerator.py
@@ -22,39 +22,53 @@ def G_default_pass() :
     print("Default password generation")
     print("length  = 13 | Contain lowercase_uppercase | Contain Numbers | Contain Symbols ")
     length = 13 
-    lowercase_uppercase = True 
+    lowercase = True 
+    uppercase = True 
     Numbers = True
     Symbols = True
 
-    Password = Generate(l=length,ul=lowercase_uppercase,n=Numbers,sy=Symbols)
+    Password = Generate(l=length,up=uppercase,lo=lowercase,n=Numbers,sy=Symbols)
     return Password
     
 
 def G_custom_pass() : 
     print("Custom password generation")
     length = int(input("Enter the length :"))
-    lowercase_uppercase = bool(input("Include lowercase and uppercase letters Enter 1 or 0 :"))
-    Numbers = bool(input("Include numbers Enter 1 or 0 :"))
-    Symbols = bool(input("Include Symbols Enter 1 or 0 :"))
+    lowercase = bool(int(input("Include lowercase letters Enter 1 or 0 :")))
+    uppercase = bool(int(input("Include uppercase letters Enter 1 or 0 :")))
+    Numbers = bool(int(input("Include numbers Enter 1 or 0 :")))
+    Symbols = bool(int(input("Include Symbols Enter 1 or 0 :")))
     
-    Password = Generate(l=length,ul=lowercase_uppercase,n=Numbers,sy=Symbols)
+    Password = Generate(l=length,up=uppercase,lo=lowercase,n=Numbers,sy=Symbols)
     return Password
 
 # Arbitrary Keyword Arguments
 def Generate(**p) :
-    #print(type(p["l"]) , type(p["ul"]) ,  type(p["n"]) , type(p["sy"]))
+    #print(type(p["l"]) , type(p["up"]) , type(p["lo"]) ,  type(p["n"]) , type(p["sy"]))
+    print(p["l"] , p["up"] , p["lo"] , p["n"] , p["sy"])
     list_numbers = [1,2,3,4,5,6,7,8,9]
     list_alpha_lowercase = list(string.ascii_lowercase)
     list_alpha_uppercase = list(string.ascii_uppercase)
     list_symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '{', '}', '[', ']', '|', ';', ':', ',', '.', '<', '>', '/', '?']
-    list_all = list_numbers + list_alpha_lowercase + list_alpha_uppercase + list_symbols
+    #list_all = list_numbers + list_alpha_lowercase + list_alpha_uppercase + list_symbols
+     
     Password = " "
-    
+    list_items = []
+    if p["up"] == True :
+         list_items.extend(list_alpha_uppercase) 
+    if p["lo"] == True :
+         list_items.extend(list_alpha_lowercase)
+    if p["n"] == True : 
+         list_items.extend(list_numbers )
+    if p["sy"] == True : 
+         list_items.extend(list_symbols)
+    if p["up"] == p["lo"] == p["n"] ==p["sy"] == False :
+         list_items.extend(list_numbers)
 
-    #print(list_all)
+    print(list_items)
     for i in range(p['l']) : 
        #print("Boucle")
-        
+        """
         r = random.randint(1,5)
         if  r == 1 :
             Password = Password + str(random.choice(list_numbers)) 
@@ -64,8 +78,8 @@ def Generate(**p) :
             Password = Password + str(random.choice(list_alpha_uppercase))
         elif r == 4 :
             Password = Password + str(random.choice(list_symbols)) 
-        else : 
-            Password = Password + str(random.choice(list_all))
+        else :  """
+        Password = Password + str(random.choice(list_items))
        
             
          
@@ -77,7 +91,8 @@ while True :
     choice = display_menu()
 
     if choice == 1 :
-       print("The password :" , G_default_pass(),type(G_default_pass())) 
+       print("The password :" , G_default_pass())
+       #type(G_default_pass()) 
        #filep.write(G_default_pass())
       # filep.newlines
         
